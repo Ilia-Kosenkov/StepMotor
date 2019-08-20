22d8a5ef52a5c990f26e6fe0bde08a8d35e5d5c6
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-08-20 16:35:37 +0300

    Added first test

---
+++ tests/DebugTests/PositioningTests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Ports;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StepMotor;
+
+namespace DebugTests
+{
+    [TestFixture]
+    public class PositioningTests
+    {
+        private SerialPort _port;
+        private IAsyncMotorFactory _factory;
+        private IAsyncMotor _motor;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _port = new SerialPort("COM1");
+            _factory = new StepMotorFactory();
+            _motor = await _factory.CreateFirstOrFromAddressAsync(_port, 1);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _motor.Dispose();
+            _port.Dispose();
+        }
+
+        [Test]
+        public async Task Test_ZeroPosition()
+        {
+            await _motor.ReturnToOriginAsync();
+            var actualPos = await _motor.GetAxisParameterAsync(CommandParam.AxisParameter.ActualPosition);
+            var internalPos = await _motor.GetAxisParameterAsync(CommandParam.AxisParameter.EncoderPosition);
+
+            Assert.Fail();
+        }
+    }
+}

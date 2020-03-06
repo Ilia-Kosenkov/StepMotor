9ff283c450201be0f0a0bf00d9efb7a68b7ed858
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-06 17:06:16 +0200

    Migrated tests to .net core

--- tests/DebugTests/PositioningTests.cs
+++ tests/DebugTests/PositioningTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Ports;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using StepMotor;

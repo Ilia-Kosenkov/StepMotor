dffad9a5338fe525384fb84948a47708c5a64879
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-27 20:16:50 +0300

    Testing new implemetation

---
+++ tests/DebugTests/PrimitiveTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Ports;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StepMotor;
+
+#nullable enable
+namespace DebugTests
+{
+    [TestFixture]
+    public class PrimitiveTests
+    {
+        [Test]
+        public async Task Test_Initialization()
+        {
+            IAsyncMotorFactory factory = new SynchronizedMotorFactory();
+            var port = new SerialPort("COM1");
+            var motor = await factory.TryCreateFromAddressAsync(port, 1);
+
+        }
+    }
+}
